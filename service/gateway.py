61471dd6178c43c1cef3a338f826f1bdbf20e11a
Varvara <varvara.malysheva@saritasa.com>
2017-08-10 16:27:09 +0700

    created connect for products

--- service/gateway.py
+++ service/gateway.py
@@ -13,9 +13,27 @@ class ShippingAPI(object):
         category(str): string name category of products
         """
 
-    @hug.object.get('/api/products', examples='name=NoteBook&category=Dell')
-    def products(self, name: str):
+    @hug.object.get('/api/shipping', examples='name=NoteBook&category=Dell')
+    def shipping(self, name: str):
         with ClusterRpcProxy(AMQP_CONFIG) as rpc:
             state = rpc.ShippingRPC.service_state(name=name)
             state2 = rpc.ShipingRPC.__doc__
         return {name: state, '42': state2}
+
+
+class ProductsAPI(object):
+    """ Class for make request on service products
+    Args:
+        AMQP_CONFIG(dict): dict object from settings file for connect
+                            to rabbitmq
+        id(int): id products
+        name(str): string name of products
+        category(str): string name category of products
+        """
+
+    @hug.object.get('/api/products', examples='name=NoteBook&category=Dell')
+    def products(self, name: str):
+        with ClusterRpcProxy(AMQP_CONFIG) as rpc:
+            state = rpc.ProductsRPC.getproduct(name=name)
+            state2 = rpc.ProductsRPC.__doc__
+        return {name: state, '100': state2}
--- service/server.py
+++ service/server.py
@@ -1,5 +1,7 @@
 import hug
 from service.gateway import ShippingAPI
+from service.gateway import ProductsAPI
 
 route = hug.route.API(__name__)
 route.object('/api/shipping')(ShippingAPI)
+route.object('/api/products')(ProductsAPI)
