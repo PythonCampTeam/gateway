895db40b2a56ee987507646823e4b60e6b7ad18b
Varvara <varvara.malysheva@saritasa.com>
2017-08-14 13:21:52 +0700

    API endpoints are working, fix docstrings

--- service/products_api.py
+++ service/products_api.py
@@ -4,18 +4,16 @@ from config.settings.common import security as security_settings
 
 
 class ProductsAPI(object):
-    """ Hello padla! Class for make request on service products
-    Args:
-        AMQP_CONFIG(dict): dict object from settings file for connect
-                            to rabbitmq
-        id(int): id products
-        name(str): string name of products
-        category(str): string name category of products
+    """Class for created requests and worked with objects Products
         """
 
     @hug.object.get('/api/products/list/{limit}', examples='limit=100')
     def products_list(self, limit: int):
-        """Get list of available product"""
+        """Get list of available product
+        Args:
+             A limit on the number of objects to be returned.
+        Returns:
+            Returns a list of products objects if the call succeeded"""
         with ClusterRpcProxy(security_settings.AMQP_CONFIG) as rpc:
             products = rpc.ProductsRPC.list_products(limit)
         return products
@@ -23,7 +21,11 @@ class ProductsAPI(object):
     @hug.object.get('/api/products/{ID}',
                     examples='ID=prod_BBZJ2ka5SKzKn7')
     def products_id(self, **kwargs):
-        """Connect to stripe and obtain information about product"""
+        """Connect to stripe and obtain information about product
+        Args:
+            ID (string) ID product to return
+        Return:
+            Returns a product object if the call succeeded."""
         ID_product = kwargs.get('ID')
         with ClusterRpcProxy(security_settings.AMQP_CONFIG) as rpc:
             product = rpc.ProductsRPC.getproduct(ID_product)
@@ -32,13 +34,19 @@ class ProductsAPI(object):
     @hug.object.post('/api/products/post/{ID}',
                      examples='ID=prod_BBZJ2ka5SKzKn7')
     def testpost1(self, **kwargs):
+        """For checking work"""
         ID_product = kwargs.get('ID')
         return {'id': ID_product}
 
     @hug.object.delete('/api/products/delete/{ID}',
                        examples='ID=prod_BBs1U1qwftIUs9')
     def product_delete(self, **kwargs):
-        """Connect to stripe and delete product"""
+        """Connect to stripe and delete product
+        Args:
+            ID (string) ID product to delete
+        Returns:
+            Returns an object with a deleted parameter on success.
+            Otherwise, this call raises an error."""
         ID_product = kwargs.get('ID')
         with ClusterRpcProxy(security_settings.AMQP_CONFIG) as rpc:
             product = rpc.ProductsRPC.delete_product(ID_product)
@@ -46,24 +54,42 @@ class ProductsAPI(object):
 
     @hug.object.post('/api/products/create/')
     def products_created(self, body):
-        """This example shows how to
-        read in post data w/ hug outside of its automatic param parsing"""
+        """Create new product
+        Args:
+            name (string) The product’s name, for the customer.
+            description (string) The product’s description for the customer.
+            attributes (lost of strings) A list of up to 5 attributes that each
+                                         SKU can provide values for
+            package_dimensions (hash) The dimensions of this product for
+                                      shipping purposes
+            metadata (hash) Set of key/value pairs that you can attach to
+                            an object. Use as category
+        Returns:
+            Returns a product object if the call succeeded.
+        """
         convert = dict(body)
         name = convert.get("name")
         description = convert.get("description")
         attributes = convert.get("attributes")
         package_dimensions = convert.get("package_dimensions")
+        metadata = convert.get("metadata")
         with ClusterRpcProxy(security_settings.AMQP_CONFIG) as rpc:
             product = rpc.ProductsRPC.create_product(name, description,
                                                      attributes,
-                                                     package_dimensions)
+                                                     package_dimensions,
+                                                     metadata)
         return product
 
     @hug.object.put('/api/products/update/')
     def product_update(self, body):
-        """Update product
+        """Updates the product
+        Note: Note that a product’s attributes are not editable.
         Args:
-            body (json) parametrs for create product"""
+            ID (string) The ID of the product to update.
+            KEY (string) The parameter to update.
+            VALUE New value for the parameter
+        Returns:
+            Returns the product object if the update succeeded."""
         kwargs = dict(body)
         ID = kwargs.get("ID")
         KEY = kwargs.get("KEY")
