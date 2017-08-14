91b863a4356e4ccdf5ddc8763902e2dfc8fdab17
Tamara Malysheva <tamara.malysheva@saritasa.com>
2017-08-14 14:01:59 +0700

    added testing

--- service/notifications_api.py
+++ service/notifications_api.py
@@ -8,22 +8,24 @@ class NotificationsAPI(object):
                     examples='name=NoteBook&category=Dell')
     def shipments(self, name: str):
         with ClusterRpcProxy(security_settings.AMQP_CONFIG) as rpc:
-            state = rpc.ShippingRPC.service_state(name=name)
-            state2 = rpc.ShipingRPC.__doc__
+            state = rpc.NotificationsRPC.testing(name=name)
+            state2 = rpc.NotificationsRPC.__doc__
         return {name: state, '42': state2}
 
-    @hug.object.post('/api/notifications/email/{email}',
-                     examples='email=tamara.mallysheva@saritasa.com')
-    def send_email(self, email):
+    @hug.object.post('/api/notifications/email/send')
+    def send_email(self, body):
         # to_email = kwargs.get("email")
+        convert = dict(body)
+        email = convert.get("email")
         with ClusterRpcProxy(security_settings.AMQP_CONFIG) as rpc:
             state = rpc.NotificationsRPC.send_email(email)
         return state
 
-    @hug.object.post('/api/notifications/sms/{number}',
-                     examples='+79994413746')
-    def send_sms(self, number):
+    @hug.object.post('/api/notifications/sms/send')
+    def send_sms(self, body):
         # number = kwargs.get("number")
+        convert = dict(body)
+        number = convert.get("number")
         with ClusterRpcProxy(security_settings.AMQP_CONFIG) as rpc:
-            state = rpc.NotificationsRPC.sms(number)
+            state = rpc.NotificationsRPC.send_sms(number)
         return state
