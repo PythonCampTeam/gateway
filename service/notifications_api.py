f3ae3cf1ffb01613fee4453a7fe18b6e73b7584f
Tamara Malysheva <tamara.malysheva@saritasa.com>
2017-08-15 19:39:07 +0700

    update notifications body

--- service/notifications_api.py
+++ service/notifications_api.py
@@ -20,20 +20,27 @@ class NotificationsAPI(object):
         """
         # to_email = kwargs.get("email")
         convert = dict(body)
-        email = convert.get("email")
+        to_email = convert.get("to_email")
+        from_email = convert.get("from_email")
+        subgect = convert.get("subject")
+        content = convert.get("content")
+
         with ClusterRpcProxy(security_settings.AMQP_CONFIG) as rpc:
-            state = rpc.NotificationsRPC.send_email(email)
+            state = rpc.NotificationsRPC.send_email(to_email, from_email,
+                                                    subgect, content)
         return state
 
-    @hug.object.post('/api/notifications/sms/send')
+    @hug.object.post('/api/notifications/Accounts/{AccountSid}/Messages',
+                     example='AccountSid=AC3adbfe0e72f9d7dc7197fefd2cab7aca')
     def send_sms(self, body):
         """This method send SMS to a customer and notify him
         Args:
-            body (dict): contain fields: to_phone, subject, body
+            body (dict): contain fields: to_phone, content
         """
         # number = kwargs.get("number")
         convert = dict(body)
-        number = convert.get("number")
+        to_phone = convert.get("to_phone")
+        content = convert.get("content")
         with ClusterRpcProxy(security_settings.AMQP_CONFIG) as rpc:
-            state = rpc.NotificationsRPC.send_sms(number)
+            state = rpc.NotificationsRPC.send_sms(to_phone, content)
         return state
