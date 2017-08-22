ff6b6d6cff43ca9cd3906a63c9e73aa4b6e2d8b3
Varvara <varvara.malysheva@saritasa.com>
2017-08-22 13:57:11 +0700

    hot fix bug

--- service/notifications_api.py
+++ service/notifications_api.py
@@ -1,33 +1,20 @@
 import hug
 import json
+import falcon
 from integration import notifications_rpc
 
 
 class NotificationsAPI(object):
-    @hug.object.get('/api/notifications',
-                    examples='name=NoteBook&category=Dell')
-    def shipments(self, name: str):
-        state = notifications_rpc.testing(name=name)
-        state2 = notifications_rpc.__doc__
-        return {name: state, '42': state2}
 
     @hug.object.post('/api/notifications/email/send')
-    def send_email(self, body):
+    def send_email(self, data):
         """This method send an email to customer and notify him.
         Args:
             body (dict):  contain fields: to_email, subject, body, from_email
         """
-        # to_email = kwargs.get("email")
-        to_email = body.get("to_email")
-        from_email = body.get("from_email")
-        subject = body.get("subject")
-        content = body.get("content")
-
-        state = notifications_rpc.send_email(subject,
-                                             content,
-                                             from_email,
-                                             to_email
-                                             )
+        state = notifications_rpc.send_email(data)
+        if state is False:
+            return falcon.HTTP_400
         return state
 
     @hug.object.post('/api/notifications/Messages/')
@@ -36,11 +23,7 @@ class NotificationsAPI(object):
         Args:
             body (dict): contain fields: to_phone, content
         """
-        # number = kwargs.get("number")
-        convert = body
-        to_phone = convert.get("to_phone")
-        content = convert.get("content")
-        state = notifications_rpc.send_sms(to_phone, content)
+        state = notifications_rpc.send_sms(body)
         return json.dumps(state)
 
 
