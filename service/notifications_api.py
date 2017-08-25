b53a06f339850f2fa7c44630a45547fc14972a58
Tamara Malysheva <tamara.malysheva@saritasa.com>
2017-08-25 12:15:18 +0700

    update gitignore

--- service/notifications_api.py
+++ service/notifications_api.py
@@ -7,29 +7,43 @@ import nameko
 class NotificationsAPI(object):
 
     @hug.object.post('/api/notifications/email/')
-    def send_email(self, body):
+    def send_email(self, to_email: hug.types.text,
+                   label: hug.types.text,
+                   from_email: hug.types.text="test@example.com",
+                   subject: hug.types.text = "PythonCamp ZooShop",
+                   name: hug.types.text = "Customer",
+                   ):
         """This method send an email to customer and notify him.
         Args:
-            data (dict):  contain fields: to_email, subject, body, from_email
+            to_emails (str) : email of customer
+            from_emails(str): email of shop
+            subject (str): subject of mail
+            name (str): name of customer
+            label (str): link to label of shipping
+        Return:
+            state (dict): return starus code of response
+
         """
-        if body is None:
+        if to_email is None or label is None:
             return {"code": falcon.HTTP_400,
                     "error":
-                    "Required fields to_phone, content"}
-        state = notifications_rpc.send_email(body)
+                    "Required fields to_email, label"}
+        state = notifications_rpc.send_email(to_email, label,
+                                             from_email, subject, name)
         return state
 
     @hug.object.post('/api/notifications/sms/')
-    def send_sms(self, body):
+    def send_sms(self, number: hug.types.text='+79994413746',
+                 content: hug.types.text="Your Order is ready"):
         """This method send SMS to a customer and notify him
         Args:
             body (dict): contain fields: to_phone, content
         """
-        if body is None:
+        if number is None:
             return {"code": falcon.HTTP_400,
                     "error":
-                    "Required fields to_phone, content"}
-        state = notifications_rpc.send_sms(body)
+                    "Required fields number"}
+        state = notifications_rpc.send_sms(number, content)
         return state
 
     @hug.exception(nameko.exceptions.RemoteError)
