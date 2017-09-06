3d5823302c27e3bc323854688b0a578e67abe255
Tamara Malysheva <tamara.malysheva@saritasa.com>
2017-09-06 15:26:12 +0700

    update docstring

--- service/notifications_api.py
+++ service/notifications_api.py
@@ -7,9 +7,12 @@ class NotificationsAPI(object):
     """Class for sended notifications for Customer"""
 
     @hug.object.post('/api/notifications/email/')
-    def send_email(self, to_email: hug.types.text,
-                   label: hug.types.text,
-                   name: hug.types.text = "Customer",
+    def send_email(
+                 self,
+                 to_email: hug.types.text,
+                 label: hug.types.text,
+                 name: hug.types.text,
+                 order,
                    ):
         """This method send an email to customer and notify him.
         Args:
@@ -17,20 +20,23 @@ class NotificationsAPI(object):
             name (str): name of customer
             label (str): link to label of shipping
         Return:
-            state (dict): return starus code of response
+            state (str): return starus code of response
         """
-        state = notifications_rpc.send_email(to_email, label, name)
+        state = notifications_rpc.send_email_with_temp(to_email, label, name, order)
         return state
 
     @hug.object.post('/api/notifications/sms/')
-    def send_sms(self, number: hug.types.text='+79994413746',
-                 content: hug.types.text="Your Order is ready"):
+    def send_sms(
+                self,
+                number: hug.types.text='+79994413746',
+                content: hug.types.text="Your Order is ready",
+                ):
         """This method send SMS to a customer and notify him
         Args:
             number (str): number of customer. Number can be valid.
             content (str): message in sms
         Return:
-            state(dict): return starus code of response
+            state(str): return starus code of response
         """
         state = notifications_rpc.send_sms(number, content)
         return state
